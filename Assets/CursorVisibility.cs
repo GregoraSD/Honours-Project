ff773b4d6308d0591f8898e6122c5c0e715cba95
Luke Powell <1402510@live.abertay.ac.uk>
2019-04-09 16:52:39 +0100

    Fixed Cursor

--- Assets/CursorVisibility.cs
+++ Assets/CursorVisibility.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class CursorVisibility : MonoBehaviour
 {
+    private void Awake()
+    {
+        UnlockCursor();
+    }
+
+    private void OnEnable()
+    {
+        UnlockCursor();
+    }
+
     private void Start()
     {
+        UnlockCursor();
+    }
+
+    private static void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 }
